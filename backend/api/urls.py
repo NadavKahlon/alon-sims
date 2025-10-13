68b4c256b0e59de1fe4b89e862ed991e3bfa80f8
Nadav Kahlon <nadavkahlon100@gmail.com>
2025-10-13 18:14:31 +0300

    backend(api): Add APi to get all role tags

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_simulations, list_simulations_topics
+from .views import list_simulations, list_simulations_topics, list_role_tags
 
 
 urlpatterns = [
     path("simulations", list_simulations),
     path("topics", list_simulations_topics),
+    path("role_tags", list_role_tags),
 ]
