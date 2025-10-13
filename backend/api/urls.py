2ad0d2f876f64995c21d69122664d8c430ae4e3e
Nadav Kahlon <nadavkahlon100@gmail.com>
2025-10-13 18:26:09 +0300

    backend(api): Add API to get week topics

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import list_simulations, list_simulations_topics, list_role_tags
+from .views import (
+    list_simulations,
+    list_simulations_topics,
+    list_role_tags,
+    list_week_topics,
+)
 
 
 urlpatterns = [
     path("simulations", list_simulations),
     path("topics", list_simulations_topics),
     path("role_tags", list_role_tags),
+    path("week_topics", list_week_topics),
 ]
