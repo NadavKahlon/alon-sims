2848def9d1f772ee493ddb0acacaef5f0d2c191b
Nadav Kahlon <nadavkahlon100@gmail.com>
2025-10-13 17:59:12 +0300

    backend(api): Add API to access simulation topics

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list_simulations
+from .views import list_simulations, list_simulation_topics
 
 
 urlpatterns = [
     path("simulations", list_simulations),
+    path("simulation_topics", list_simulation_topics),
 ]
